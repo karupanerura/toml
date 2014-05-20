e250609b23f38152fd88521aac342a010ed46b2e
karupanerura <karupa@cpan.org>
2014-05-20 12:44:38 +0900

    license move to doc

--- lib/TOML.pm
+++ lib/TOML.pm
@@ -4,20 +4,6 @@ package TOML;
 # TOML - Parser for Tom's Obvious, Minimal Language.
 #
 # Copyright (C) 2013 Darren Chamberlain <darren@cpan.org>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; version 2.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02111-1301 USA
 # -------------------------------------------------------------------
 
 use strict;
@@ -194,6 +180,22 @@ string.
 =head1 SEE ALSO
 
 L<TOML::Parser>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; version 2.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02111-1301 USA
 
 =head1 AUTHOR
 
